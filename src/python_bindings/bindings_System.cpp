1b40ae0936ed5c386a6152659fd7771106b7b284
ac133451 <peter.somers@isys.uni-stuttgart.de>
2021-01-22 18:16:32 +0100

    added track monocular and changed PYTHON to PYTHON_EXECUTABLE

--- src/python_bindings/bindings_System.cpp
+++ src/python_bindings/bindings_System.cpp
@@ -1,4 +1,6 @@
 #include <System.h>
+#include <ImuTypes.h>
+
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
@@ -8,18 +10,25 @@ PYBIND11_MODULE(ORBSLAM3, m){
             .def(py::init<const std::string &, const std::string &, const ORB_SLAM3::System::eSensor, const bool>(),
                     py::arg("strVocFile"), py::arg("strSettingsFile"), py::arg("sensor"), py::arg("bUseViewer"),
                     R"mydelimiter(
-                         Initialize the SLAM system. It launches the Local Mapping, Loop Closing and Viewer threads.\n
-                         Parameters\n
-                        -----------\n
-                        string strVocFile: path to the vocabulary file.\n
-                        string strSettingsFile: path to the settings file which contains data like the camera parameters\n
-                        eSensor sensor: What mode to run the system in (Monocular, stereo, with imu, etc...)\n
-                        bool bUseViewer: Whether or not to activate the built-in GUI for ORBSLAM
-                         )mydelimiter")
+                    Initialize the SLAM system. It launches the Local Mapping, Loop Closing and Viewer threads.\n
+                    Parameters\n
+                    -----------\n
+                    string strVocFile: path to the vocabulary file.\n
+                    string strSettingsFile: path to the settings file which contains data like the camera parameters\n
+                    eSensor sensor: What mode to run the system in (Monocular, stereo, with imu, etc...)\n
+                    bool bUseViewer: Whether or not to activate the built-in GUI for ORBSLAM
+                    )mydelimiter")
             .def("ActivateLocalizationMode", &ORB_SLAM3::System::ActivateLocalizationMode,
                  R"mydelimiter(
                         This stops local mapping thread (map building) and performs only camera tracking.
-                         )mydelimiter");
+                         )mydelimiter")
+            .def("TrackMonocular", &ORB_SLAM3::System::TrackMonocular,
+                 py::arg("im"), py::arg("timestamp"), py::arg("vImuMeas")=std::vector<ORB_SLAM3::IMU::Point>(), py::arg("filename")="",
+                 R"mydelimiter(
+                 Proccess the given monocular frame and optionally imu data.\n
+                 Input images: RGB (CV_8UC3) or grayscale (CV_8U). RGB is converted to grayscale.\n
+                 Returns the camera pose (empty if tracking fails).
+                 )mydelimiter");
 
     py::enum_<ORB_SLAM3::System::eSensor>(m, "eSensor")  // This can be used from python with ORBSLAM3.eSensor.######
             .value("MONOCULAR", ORB_SLAM3::System::eSensor::MONOCULAR)
